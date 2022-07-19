b4a1e22743029270779aae8561a52a0b3d4f09be
Jose A Mendez Santiago <jamsmendez02@gmail.com>
2022-07-19 13:15:18 -0500

    Version funcional

---
+++ README.md
@@ -0,0 +1,14 @@
+# Instalacion
+
+```
+cp wallpapers-random.sh ~/.local/bin/wallpapers-random.sh
+
+sudo cp wallpapers-random.service /etc/systemd/system/wallpapers-random.service
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable wallpapers-random.service
+```
+
+### Fuente
+https://gist.github.com/himalay/37031c529b1e4a04ad31
---
+++ wallpapers-random.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+
+# $DISPLAY not found ... try again
+while true; do
+  export DISPLAY=":0.0"
+
+  # images path
+  DIR="/home/USUARIO/Pictures/Wallpapers/release"
+
+  if [[ -n "$DISPLAY" ]]; then 
+    img=(`find $DIR -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
+    while true; do
+      # feh --no-fehbg --bg-fill "${img[$RANDOM % ${#img[@]} ]}"
+      feh --bg-fill "${img[$RANDOM % ${#img[@]} ]}"
+      sleep 30m
+    done
+  else
+    echo "Waiting DISPLAY ..."
+    sleep 1s
+  fi
+done
