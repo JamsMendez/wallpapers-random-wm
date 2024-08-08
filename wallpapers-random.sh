2e859ffe331e88c0869f014eb237f7601c9441b3
Jose A. Mendez Santiago <jamsmendez02@gmail.com>
2024-08-07 23:00:18 -0600

    version to Gnome Desktop

--- wallpapers-random.sh
+++ wallpapers-random.sh
@@ -5,15 +5,21 @@ while true; do
   export DISPLAY=":0.0"
 
   # images path
-  DIR="/home/USER/Pictures/Wallpapers/release"
+  DIR="/opt/wallpapers"
 
   if [[ -n "$DISPLAY" ]]; then 
     img=(`find $DIR -name '*' -exec file {} \; | grep -o -P '^.+: \w+ image' | cut -d':' -f1`)
     while true; do
       # feh --no-fehbg --bg-fill "${img[$RANDOM % ${#img[@]} ]}"
-      feh --bg-fill "${img[$RANDOM % ${#img[@]} ]}"
-      # sleep 30 minites ... change to 15m, 10m, etc
-      sleep 30m
+      # feh --bg-fill "${img[$RANDOM % ${#img[@]} ]}"
+      if [[ `gsettings get org.gnome.desktop.interface color-scheme` =~ 'dark' ]]; then
+        gsettings set org.gnome.desktop.background picture-uri-dark "file://${img[$RANDOM % ${#img[@]} ]}"
+      else 
+        gsettings set org.gnome.desktop.background picture-uri "file://${img[$RANDOM % ${#img[@]} ]}"
+        # sleep 30 minites ... change to 15m, 10m, etc
+      fi
+      echo "..... changing wallpaper"
+      sleep 15s
     done
   else
     echo "Waiting DISPLAY ..."
